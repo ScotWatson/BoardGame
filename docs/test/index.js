5ead5e9df1692918f3a7cd6fcb7db07b672fcb96
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2023-12-31 22:12:21 -0500

    Update index.js

--- docs/test/index.js
+++ docs/test/index.js
@@ -58,8 +58,225 @@ function createRequestPOST(endpoint, body) {
   });
 }
 
+class MenuTiles {
+  #elemMain;
+  #rootMain;
+  constructor() {
+    this.#elemMain = document.createElement("menu-tiles");
+    this.#rootMain = this.#elemMain.attachShadow();
+    this.#elemMain.style.display = "flex";
+    this.#elemMain.style.flexDirection = "row";
+    this.#elemMain.style.flexWrap = "wrap";
+    this.#elemMain.style.justifyContent = "space-around";
+    this.#elemMain.style.alignItems = "center";
+    this.#elemMain.style.alignContent = "space-around";
+  }
+  addTile() {
+    const divNewTile = document.createElement("div");
+    this.#rootMain.appendChild(divNewTile);
+    divNewTile.style.display = "block";
+    divNewTile.style.width = "20%";
+    return divNewTile;
+  }
+  removeTile() {
+    
+  }
+}
+
+class AppNavigation {
+  #elemMain;
+  #rootMain;
+  #divBreadcrumbs;
+  #divMainTitle;
+  #divPenultimateTitle;
+  #divUltimateTitle;
+  #arrBreadcrumbs;
+  #btnForward;
+  #divTopLayout;
+  constructor({ title, shortTitle }) {
+    this.#elemMain = document.createElement("app-navigation");
+    this.#rootMain = this.#elemMain.attachShadow();
+    this.#elemMain.style.backgroundColor = "grey";
+    this.#divBreadcrumbs = document.createElement("div");
+    this.#rootMain.appendChild(this.#divBreadcrumbs);
+    this.#divBreadcrumbs.style.backgroundColor = "grey";
+    this.#btnBreadcrumbs.style.position = "absolute";
+    this.#btnBreadcrumbs.style.left = "95%";
+    this.#divBreadcrumbs.style.width = "100%";
+    this.#divBreadcrumbs.style.height = "10%";
+    this.#divMainTitle = null;
+    this.#divPenultimateTitle = null;
+    this.#divUltimateTitle = null;
+    this.#arrBreadcrumbs = [];
+    this.#divTopLayout = this.addLayout({ title, shortTitle });
+  }
+  get element() {
+    return this.#elemMain;
+  }
+  get topLayout() {
+    return this.#divTopLayout;
+  }
+  addLayout({ title, shortTitle }) {
+    const divLayout = document.createElement("div");
+    for (const child of divMain.children) {
+      child.style.display = "none";
+    }
+    this.#arrBreadcrumbs.push({ title, shortTitle, divLayout });
+    this.#redrawBreadcrumbs();
+    return divLayout;
+  }
+  closeLayout() {
+    { title, shortTitle, divLayout } = this.#arrBreadcrumbs.pop();
+    this.#redrawBreadcrumbs();
+    divLayout.remove();
+  }
+  #redrawBreadcrumbs() {
+    // Remove all elements from the breadcrumbs
+    for (const child of this.#divBreadcrumbs.children) {
+      child.remove();
+    }
+    // Layout was just added, length cannot be 0
+    switch (this.#arrBreadcrumbs.length) {
+      case 1:
+        // Create main title element (full width)
+        this.#divMainTitle = document.createElement("div");
+        this.#divBreadcrumbs.appendChild(this.#divMainTitle);
+        this.#divMainTitle.style.width = "100%";
+        this.#divMainTitle.style.height = "100%";
+        this.#divMainTitle.style.boxSizing = "content-box";
+        this.#divMainTitle.style.border = "1px solid black";
+        this.#divPenultimateTitle = null;
+        this.#divUltimateTitle = null;
+        this.#btnHistory = null;
+        // Insert Titles
+        this.#divMainTitle.appendChild(document.createTextNode(this.#arrBreadcrumbs[0].title));
+        break;
+      case 2:
+        // Create main title element (short) & ultimate title element
+        this.#divMainTitle = document.createElement("div");
+        this.#divBreadcrumbs.appendChild(this.#divMainTitle);
+        this.#divMainTitle.style.width = "15%";
+        this.#divMainTitle.style.height = "100%";
+        this.#divMainTitle.style.boxSizing = "content-box";
+        this.#divMainTitle.style.border = "1px solid black";
+        this.#divPenultimateTitle = null;
+        this.#divUltimateTitle = document.createElement("div");
+        this.#divBreadcrumbs.appendChild(this.#divUltimateTitle);
+        this.#divUltimateTitle.style.position = "absolute";
+        this.#divUltimateTitle.style.left = "15%";
+        this.#divUltimateTitle.style.width = "85%";
+        this.#divUltimateTitle.style.height = "100%";
+        this.#divUltimateTitle.style.boxSizing = "content-box";
+        this.#divUltimateTitle.style.border = "1px solid black";
+        this.#btnHistory = null;
+        // Insert Titles
+        this.#divMainTitle.appendChild(document.createTextNode(this.#arrBreadcrumbs[0].shortTitle));
+        this.#divUltimateTitle.appendChild(document.createTextNode(this.#arrBreadcrumbs[1].title));
+        break;
+      default:
+        // Create main title element (short) & ultimate title element
+        this.#divMainTitle = document.createElement("div");
+        this.#divBreadcrumbs.appendChild(this.#divMainTitle);
+        this.#divMainTitle.style.position = "absolute";
+        this.#divMainTitle.style.left = "0%";
+        this.#divMainTitle.style.width = "15%";
+        this.#divMainTitle.style.height = "100%";
+        this.#divMainTitle.style.boxSizing = "content-box";
+        this.#divMainTitle.style.border = "1px solid black";
+        this.#divPenultimateTitle = document.createElement("div");
+        this.#divBreadcrumbs.appendChild(this.#divPenultimateTitle);
+        this.#divPenultimateTitle.style.position = "absolute";
+        this.#divPenultimateTitle.style.left = "20%";
+        this.#divPenultimateTitle.style.width = "15%";
+        this.#divPenultimateTitle.style.height = "100%";
+        this.#divPenultimateTitle.style.boxSizing = "content-box";
+        this.#divPenultimateTitle.style.border = "1px solid black";
+        this.#divUltimateTitle = document.createElement("div");
+        this.#divBreadcrumbs.appendChild(this.#divUltimateTitle);
+        this.#divUltimateTitle.style.position = "absolute";
+        this.#divUltimateTitle.style.left = "35%";
+        this.#divUltimateTitle.style.width = "60%";
+        this.#divUltimateTitle.style.height = "100%";
+        this.#divUltimateTitle.style.boxSizing = "content-box";
+        this.#divUltimateTitle.style.border = "1px solid black";
+        this.#btnHistory = document.createElement("div");
+        this.#divBreadcrumbs.appendChild(this.#btnHistory);
+        this.#btnHistory.style.position = "absolute";
+        this.#btnHistory.style.left = "95%";
+        this.#btnHistory.style.width = "5%";
+        this.#btnHistory.style.height = "100%";
+        this.#btnHistory.style.boxSizing = "content-box";
+        this.#btnHistory.style.border = "1px solid black";
+        // Insert Titles
+        this.#divMainTitle.appendChild(document.createTextNode(this.#arrBreadcrumbs[0].shortTitle));
+        this.#divPenultimateTitle.appendChild(document.createTextNode(this.#arrBreadcrumbs[this.#arrBreadcrumbs.length - 2].shortTitle));
+        this.#divUltimateTitle.appendChild(document.createTextNode(this.#arrBreadcrumbs[this.#arrBreadcrumbs.length - 1].title));
+    }
+    if (this.#divMainTitle !== null) {
+      this.#divMainTitle.addEventListener("click", function (evt) {
+        while (this.#arrBreadcrumbs.length > 1) {
+          this.closeLayout();
+        }
+      });
+    }
+    if (this.#divPenultimateTitle !== null) {
+      this.#divPenultimateTitle.addEventListener("click", function (evt) {
+        this.closeLayout();
+      });
+    }
+    if (this.#btnHistory !== null) {
+      this.#btnHistory.addEventListener("click", function (evt) {
+      
+      });
+    }
+  }
+}
+
 function start( [ evtWindow ] ) {
   try {
+    // Create full window div for full control. Height must be resized by JS.
+    document.body.backgroundColor = "black";
+    document.body.margin = "0px";
+    const divMain = document.createElement("div");
+    document.body.appendChild(divMain);
+    divMain.style.width = "100%";
+    divMain.style.overflow = "hidden hidden";
+    window.addEventListener("resize", function (evt) {
+      divMain.style.display = "none";
+      let timer;
+      clearTimeout(timer);
+      timer = setTimeout(redraw, 200);
+    });
+    function setMainHeight() {
+      divMain.style.display = "block";
+      divMain.style.height = window.innerHeight + "px";
+    }
+    redraw();
+    // Create navigation control
+    const myNav = new AppNavigation({
+      baseElement: document.body,
+    });
+    divMain.appendChild(myNav.element);
+    myNav.addLayout({
+      title: "",
+      shortTitle: "",
+    });
+    function draw( { mainTitle, mainShortTitle} ) {
+      const divMainTitle = document.createElement("div");
+      divTitleBreadcrumbs.appendChild(divMainTitle);
+      divMainTitle.style.width = "100%";
+      divMainTitle.style.height = "100%";
+      divMainTitle.appendChild(document.createTextNode(mainTitle));
+      const divMainScreen = document.createElement("div");
+      divMain.appendChild(divMainScreen);
+      divMainScreen.style.backgroundColor = "white";
+      divMainScreen.style.width = "100%";
+      divMainScreen.style.height = "90%";
+      return divMainScreen;
+    }
+
+    
+    
     const urlSelf = new URL(self.location);
     const urlServiceWorker = new URL("./sw.js", urlSelf);
     const urlServiceWorkerScope = new URL("./", urlSelf);
