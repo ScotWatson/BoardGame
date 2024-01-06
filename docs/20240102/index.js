569146e6944f925758ce38aa3228e73ed78bd471
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-01-05 22:15:52 -0500

    Update index.js

--- docs/20240102/index.js
+++ docs/20240102/index.js
@@ -145,7 +145,7 @@ class AppNavigation {
   get element() {
     return this.#elemMain;
   }
-  addLayout({ title, shortTitle }) {
+  addLayout({ title, shortTitle, refresh }) {
     const divLayout = document.createElement("div");
     for (const child of this.#divContent.children) {
       child.style.display = "none";
@@ -156,7 +156,10 @@ class AppNavigation {
     this.#arrBreadcrumbs.push({ title, shortTitle, divLayout });
     this.#divContent.appendChild(divLayout);
     this.#redrawBreadcrumbs();
-    return divLayout;
+    const contents = new DocumentFragment();
+    refresh(contents).then(function () {
+      divLayout.appendChild(contents);
+    });
   }
   closeLayout() {
     const { title, shortTitle, divLayout } = this.#arrBreadcrumbs.pop();
