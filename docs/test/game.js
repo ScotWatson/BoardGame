115d4975c645c2a93edb8df2b2ce3d4c08c62c14
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-01-17 19:37:38 -0500

    Create game.js

---
+++ docs/test/game.js
@@ -0,0 +1,63 @@
+/*
+(c) 2024 Scot Watson  All Rights Reserved
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+const initPageTime = performance.now();
+
+const asyncWindow = new Promise(function (resolve, reject) {
+  window.addEventListener("load", function (evt) {
+    resolve(evt);
+  });
+});
+
+(async function () {
+  try {
+    const modules = await Promise.all( [ asyncWindow ] );
+    start(modules);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+})();
+
+// Creates a GET Request to the specified endpoint
+function createRequestGET(endpoint) {
+  return new self.Request(endpoint, {
+    method: "GET",
+    headers: [],
+    mode: "cors",
+    credentials: "same-origin",
+    cache: "default",
+    redirect: "follow",
+    referrer: "about:client",
+    referrerPolicy: "",
+    integrity: "",
+    keepalive: "",
+    signal: null,
+    priority: "auto",
+  });
+}
+
+// Creates a POST Request to the specified endpoint w/ the specified body
+function createRequestPOST(endpoint, body) {
+  return new self.Request(endpoint, {
+    method: "POST",
+    headers: [],
+    mode: "cors",
+    body: body,
+    credentials: "same-origin",
+    cache: "default",
+    redirect: "follow",
+    referrer: "about:client",
+    referrerPolicy: "",
+    integrity: "",
+    keepalive: "",
+    signal: null,
+    priority: "auto",
+  });
+}
+
+function start() {
+
+}
