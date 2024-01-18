340692c0d4241ac76b5504e2b8df61817e3c04b3
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-01-17 20:02:59 -0500

    Update game.js

--- docs/test/game.js
+++ docs/test/game.js
@@ -3,7 +3,7 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-importScripts("ui.js");
+importScripts("./ui.js");
 
 const initPageTime = performance.now();
 
@@ -61,5 +61,13 @@ function createRequestPOST(endpoint, body) {
 }
 
 function start() {
-
+  try {
+    const urlSelf = new URL(self.location);
+    const url = urlSelf.searchParams("url");
+    const gameId = urlSelf.searchParams("gameId");
+    const token = urlSelf.searchParams("token");
+    const nav = new AppNavigation();
+  } catch (e) {
+    console.error(e);
+  }
 }
